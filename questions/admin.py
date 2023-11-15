3dd028856fb50208cac37141403eee4e36f58cc5
inna-tuzhikova <inna-tuzhikova@github.com>
2023-11-15 19:00:29 +0300

    admin site

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Answer, Tag
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, dict(fields=['caption', 'text', 'tags', 'rating'])),
+        ('Meta', dict(fields=['author', 'created'])),
+    ]
+    inlines = [AnswerInline]
+    list_display = ['caption', 'text', 'author', 'rating', 'created']
+    list_filter = ['created']
+    search_fields = ['caption', 'text']
+
+
+admin.site.register(Question, QuestionAdmin)
